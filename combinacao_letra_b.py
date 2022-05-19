b6f601b4eaf3c6015b39c3d12d51ffedde725faa
Yago Walter <yago.walter_7@hotmail.com>
2022-05-19 13:20:40 -0300

    Delete combinacao_letra_b.py

--- combinacao_letra_b.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations
-
-banco = ['SQL','NoSQL','GIS']
-programacao = ['Python','Java','C','PHP']
-eng = ['Metodos','Processos']
-temp = combinations(banco+programacao+eng,1)
-for i in list(temp):
-    print (i)
-    
